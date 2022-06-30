aa946d97296979364713047e457f9cd287b1d157
DevMan3158 <e.giner@codeur.online>
2022-06-30 13:40:18 +0200

    Ajout page twig

--- src/Controller/TheHomeController.php
+++ src/Controller/TheHomeController.php
@@ -2,31 +2,46 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class TheHomeController
+class TheHomeController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<h1>Accueil</h1>');
+
+        $produit = [
+
+            ['GPU' => 'Rtx 3070', 'Constructeur' => 'Msi'],
+            ['GPU' => 'Rtx 3090 Ti Fe', 'Constructeur' => 'Nvidia'],  
+           
+
+        ];
+
+
+        return $this->render('pages/homepage.html.twig',[
+
+            'title' => 'Accueil',
+            'produit' => $produit,
+
+        ]);
     }
 
     #[Route('/browse/{slug}')]
-    public function browse(string $slug =null): Response
+    public function browse(string $slug = null): Response
     {
-       
-        if($slug){
 
-          $title = '<h2>Produits : </h2>'.u(str_replace('-',' ', $slug))->title(true);  
+        if ($slug) {
 
-        }else{
+            $title = '<h2>Produits : </h2>' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
 
             $title = '<h2>Tous les produits</h2>';
         }
-        
+
         return new Response($title);
     }
 
